085413dcb5e4b374e0a7e6615ab5c13a44188eac
Miguel T Rivera <mrivera108@outlook.com>
2020-12-18 23:44:03 -0800

    Create subreddit dictionary from file

--- main.py
+++ main.py
@@ -8,10 +8,36 @@ def is_connected(host='https://google.com'):
     except:
         return False
 
+def sub_exists(r, sub):
+    try:
+        r.subreddits.search_by_name(sub, exact=True)
+        return True
+    except:
+        return False
+
+def read_file(r, file):
+    dict = {}
+    with open(file, "r") as infile:
+        line = infile.readline()
+        while line:
+            sub = line.strip('\n')
+            if sub_exists(r, sub):
+                dict[sub] = 0
+            line = infile.readline()
+    return dict
+
+
+
 def main():
     if not is_connected():
         print('No internet connection. Script cannot run while offline.')
         return
     print('Success! Internet connection established.')
+
+    reddit = praw.Reddit("purge")
+
+    subs = read_file(reddit, "subreddits.txt")
+    print(subs)
+
 
 main()
